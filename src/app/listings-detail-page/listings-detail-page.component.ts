fcb7ff662dff1f23b19d71c104bbe0072a70f1ff
Cornell Reddick <cornellreddick@outlook.com>
2023-08-13 10:13:30 -0400

    Set up the listing detail page

--- src/app/listings-detail-page/listings-detail-page.component.ts
+++ src/app/listings-detail-page/listings-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listings-detail-page',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings-detail-page.component.css']
 })
 export class ListingsDetailPageComponent implements OnInit {
+  listing: Listing | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, 
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
   }
 
 }
